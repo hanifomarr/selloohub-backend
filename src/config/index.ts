8be5a0bcf8bb19b371d6bc904c9abb5391b43251
Hanif Omar <hanif.omarr@gmail.com>
2025-07-22 18:05:02 +0800

    feat(cors): add CORS support with configurable options

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,14 @@
-import dotenv from "dotenv";
+/**
+ * Node modules
+ */
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  WHITELIST: process.env.WHITELIST,
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,51 @@
-import express, { Request, Response } from "express";
-import config from "@/config";
+/**
+ * Node modules
+ */
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
+/**
+ * Custom modules
+ */
+import config from '@/config';
+import { CorsOptions } from 'cors';
+
+/**
+ * Server setup
+ */
 const app = express();
 
+// Configure CORS options
+const corsOptions: CorsOptions = {
+  origin(requestOrigin, callback) {
+    if (
+      config.NODE_ENV === 'development' ||
+      !requestOrigin ||
+      config.WHITELIST?.includes(requestOrigin)
+    ) {
+      callback(null, true);
+    } else {
+      console.warn(`CORS error: ${requestOrigin} is not allowed by CORS.`);
+      callback(
+        new Error(`CORS error: ${requestOrigin} is not allowed by CORS`),
+      );
+    }
+  },
+};
+
+//Apply CORS middleware
+app.use(cors(corsOptions));
+
+//enable JSON request body parsing
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("API is running");
+//enable URL-encoded request body parsing
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('API is running');
 });
 
 app.listen(config.PORT, () =>
-  console.log(`Server running on port ${config.PORT}`)
+  console.log(`Server running on port ${config.PORT}`),
 );
