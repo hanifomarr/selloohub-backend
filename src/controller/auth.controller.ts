9e0de860b054d3119dccb8ca6dd4693279c884a3
Hanif Omar <hanif.omarr@gmail.com>
2025-10-07 01:54:42 +0800

    feat(auth): implement login DTO and enhance login validation (#4)

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,3 +1,4 @@
+import { LoginDto } from "@/dto/auth/login-user.dto";
 import { RegisterDto } from "@/dto/auth/register-user.dto";
 import { authService } from "@/service/auth.service";
 import { plainToInstance } from "class-transformer";
@@ -6,8 +7,8 @@ import { Request, Response } from "express";
 
 export const register = async (req: Request, res: Response) => {
 
-    const authDto = plainToInstance(RegisterDto, req.body)
-    const error = await validate(authDto)
+    const registerDto = plainToInstance(RegisterDto, req.body)
+    const error = await validate(registerDto)
 
     if (error.length > 0) {
 
@@ -19,7 +20,7 @@ export const register = async (req: Request, res: Response) => {
     }
 
     try {
-        const result = await authService.Register(authDto);
+        const result = await authService.Register(registerDto);
         res.status(201).json(result)
     } catch (error: any) {
         res.status(400).json({ message: error.message });
@@ -27,6 +28,16 @@ export const register = async (req: Request, res: Response) => {
 }
 
 export const login = async (req: Request, res: Response) => {
+
+    const loginDto = plainToInstance(LoginDto, req.body)
+    const error = await validate(loginDto)
+    if (error.length > 0) {
+        const formatedError = error.map(err => {
+            return Object.values(err.constraints || {})
+        }).flat()
+        return res.status(400).json({ status: "error", error: formatedError })
+    }
+
     try {
         const result = await authService.Login(req.body)
         res.status(201).json(result)
--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,4 +1,5 @@
 import { AppDataSource } from "@/data-source"
+import { LoginDto } from "@/dto/auth/login-user.dto";
 import { RegisterDto } from "@/dto/auth/register-user.dto";
 import { ResponseUserDto } from "@/dto/auth/response-user.dto";
 import { User } from "@/entity/user.entity";
@@ -27,7 +28,7 @@ class AuthService {
         return new ResponseUserDto(savedUser, token)
     }
 
-    async Login(data: { email: string, password: string }) {
+    async Login(data: LoginDto) {
 
         const user = await userRepository.findOneBy({ email: data.email })
         if (!user) throw new Error("Invalid email or password")
@@ -35,9 +36,8 @@ class AuthService {
         const isPasswordValid = await comparePassword(data.password, user.password)
         if (!isPasswordValid) throw new Error("Invalid email or password")
 
-
         const token = await generateToken({ id: user.id, email: user.email })
-        return { user, token }
+        return new ResponseUserDto(user, token)
 
     }
 }
